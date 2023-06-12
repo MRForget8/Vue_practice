0206daec0fde2ff39e72acc150bd3ac4745283b9
MRForget8 <130636311+MRForget8@users.noreply.github.com>
2023-06-12 11:01:28 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="app">
+    <div v-if="loggedIn">
+      <button @click="logout">Logout</button>
+    </div>
+    <div v-else>
+      <!-- 登录组件 -->
+      <Login @login-success="handleLoginSuccess" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Login from './components/LoginForm.vue';
+
+export default {
+  components: {
+    Login
+  },
+  data() {
+    return {
+      loggedIn: false
+    };
+  },
+  methods: {
+    handleLoginSuccess() {
+      this.loggedIn = true;
+    },
+    logout() {
+      this.loggedIn = false;
+    }
+  }
+};
+</script>
+
+<style>
+.app {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+button {
+  padding: 10px 20px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+  margin-top: 10px;
+}
+</style>
---
+++ LoginForm.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="login-container">
+      <h2>Login</h2>
+      <form @submit.prevent="login">
+        <div class="form-group">
+          <label for="username">Username</label>
+          <input type="text" id="username" v-model="username" required>
+        </div>
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input type="password" id="password" v-model="password" required>
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        username: '',
+        password: ''
+      };
+    },
+    methods: {
+      login() {
+        // 在这里编写登录逻辑
+        // 可以通过发送请求到服务器验证用户名和密码
+        // 如果验证成功，可以进行跳转或其他操作
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .login-container {
+    max-width: 400px;
+    margin: 0 auto;
+    padding: 20px;
+  }
+  
+  .form-group {
+    margin-bottom: 10px;
+  }
+  
+  label {
+    display: block;
+  }
+  
+  input {
+    width: 100%;
+    padding: 5px;
+    font-size: 14px;
+  }
+  
+  button {
+    padding: 10px 20px;
+    background-color: #007bff;
+    color: #fff;
+    border: none;
+    cursor: pointer;
+  }
+  </style>
+  
